27643f5e0b8d43fe80af95465c5316e2d1d16dcd
Israel <jacobisraelreyessalas@gmail.com>
2025-07-04 19:56:33 -0600

    The action listener and validation form were added

--- src/main/java/com/tienda/interfaces/SignIn.java
+++ src/main/java/com/tienda/interfaces/SignIn.java
@@ -2,7 +2,9 @@
 package com.tienda.interfaces;
 
 import jakarta.persistence.EntityManagerFactory;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -11,7 +13,7 @@ public class SignIn {
     private final int WIDTH = 150;
     private final int HEIGHT = 30;
     
-    private JTextField fieldName, fieldMaternalName, fieldPaternalName;
+    private JTextField fieldName, fieldMaternalName, fieldPaternalName, fieldEmail;
     private JTextField fieldNumberPhone, fieldNickname, fieldPassword;
     
     private JButton signIn;
@@ -33,6 +35,7 @@ public class SignIn {
         fieldMaternalName = new JTextField(30);
         fieldNumberPhone = new JTextField(30);
         fieldNickname = new JTextField(30);
+        fieldEmail = new JTextField(30);
         fieldPassword = new JTextField(30);
         signIn = new JButton("Sign In");
         
@@ -41,18 +44,47 @@ public class SignIn {
         fieldMaternalName.setBounds(20, 100, WIDTH, HEIGHT);
         fieldNumberPhone.setBounds(20, 140, WIDTH, HEIGHT);
         fieldNickname.setBounds(20, 180, WIDTH, HEIGHT);
-        fieldPassword.setBounds(20, 220, WIDTH, HEIGHT);
-        signIn.setBounds(20, 260, WIDTH, HEIGHT);
+        fieldEmail.setBounds(20, 220, WIDTH, HEIGHT);
+        fieldPassword.setBounds(20, 260, WIDTH, HEIGHT);
+        signIn.setBounds(20, 300, WIDTH, HEIGHT);
+        
+        signIn.addActionListener( listenerSignIn() );
         
         panel.add(fieldName);
         panel.add(fieldPaternalName);
         panel.add(fieldMaternalName);
         panel.add(fieldNumberPhone);
         panel.add(fieldNickname);
+        panel.add(fieldEmail);
         panel.add(fieldPassword);
         panel.add(signIn);
     }
     
+    private ActionListener listenerSignIn () {
+        return action -> {
+            String name = fieldName.getText();
+            String paternalName = fieldPaternalName.getText();
+            String maternalName = fieldMaternalName.getText();
+            String numberPhone = fieldNumberPhone.getText();
+            String nickname = fieldNickname.getText();
+            String password = fieldPassword.getText();
+            String email = fieldEmail.getText();
+            
+            if ( isValidForm( name, paternalName, maternalName, numberPhone, nickname, password, email) ) {
+                
+            } else {
+                showMessage( "There are empty fields" );
+            }
+        };
+    }
     
+    private Boolean isValidForm (String name, String paternalName, String maternalName, String numberPhone, String nickname, String password, String email) {
+        return !name.equals("") && !paternalName.equals("") && !maternalName.equals("") &&
+                !numberPhone.equals("") && !nickname.equals("") && !password.equals("") && !email.equals("") ;
+    }
+    
+    private void showMessage( String message ) {
+        JOptionPane.showMessageDialog(null, message);
+    }    
     
 }
