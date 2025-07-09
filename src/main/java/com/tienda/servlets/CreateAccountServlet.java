e09e343f40316f6f49f7a1cdd776c9dfd7d46d92
Israel <jacobisraelreyessalas@gmail.com>
2025-07-08 18:35:51 -0600

    Servlent for Create Account was added

---
+++ src/main/java/com/tienda/servlets/CreateAccountServlet.java
@@ -0,0 +1,11 @@
+
+package com.tienda.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+@WebServlet( name="CreateAccountServlet", urlPatterns ={"/CreateAccountServlet"} )
+public class CreateAccountServlet extends HttpServlet{
+    
+}
