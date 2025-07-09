7b515c3edacb54633471bba6a2a59767eb365a88
Israel <jacobisraelreyessalas@gmail.com>
2025-07-08 20:25:20 -0600

    Complete constructor added

--- src/main/java/com/store/entities/AccountRole.java
+++ src/main/java/com/store/entities/AccountRole.java
@@ -1,5 +1,5 @@
 
-package com.tienda.entities;
+package com.store.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -32,9 +33,10 @@ public class AccountRole implements Serializable {
         this.name = name;
     }
 
-    public AccountRole(Integer id, String name) {
+    public AccountRole(Integer id, String name, Set<Account> account) {
         this.id = id;
         this.name = name;
+        this.account = account;
     }
 
     public Set<Account> getAccount() {
