fc2a0e4f38e5d8d2c5ff550ae2f81e6edca3d06a
Israel <jacobisraelreyessalas@gmail.com>
2025-07-08 18:33:27 -0600

    New persistence unit init

--- src/main/java/com/tienda/servlets/StartupServlet.java
+++ src/main/java/com/tienda/servlets/StartupServlet.java
@@ -1,17 +1,32 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
 package com.tienda.servlets;
 
 import com.tienda.persistence.controllers.PersistenceControllerFactory;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import jakarta.persistence.EntityManagerFactory;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
-@WebServlet(name = "StartupServlet", urlPatterns = {}, loadOnStartup=1)
-public class StartupServlet extends HttpServlet {
+/**
+ *
+ * @author compi
+ */
+@WebListener
+public class StartupServlet implements ServletContextListener {
+    
+     @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        PersistenceControllerFactory.getInstance();
+    }
 
-   @Override
-   public void init() throws ServletException {
-       PersistenceControllerFactory.getInstance();
-   }
-
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        EntityManagerFactory emf = PersistenceControllerFactory.getInstance().getManagerFactory();
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
 }
